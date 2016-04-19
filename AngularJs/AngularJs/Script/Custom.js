bc1fe0a21dab9285fab8eab9b1140786ab3ee109
Pasupathi Ganesan <Pasupathi Ganesan>
2016-04-20 00:40:57 +0530

    First angularjs File

---
+++ AngularJs/AngularJs/Script/Custom.js
@@ -0,0 +1,25 @@
+﻿/// <reference path="angular.js" />
+
+var myapp = angular.module("mymodel", [])
+                    .controller("mycontroller", function ($scope, $http, $window) {
+                        var successcallback = function (response) {
+                            $scope.Loginvalue = response.data;
+                            if ($scope.Loginvalue == 1) {
+                                $scope.Successmessage = "Hello Welcome To AngularJs";
+                                $window.location.href = 'Home.html';
+                                
+                            }
+                            else
+                                $scope.ErrorMessage = "Invalid userName and Password";
+
+                        };
+                        var errorcallback = function (Error) {
+                            $scope.ErrorMessage = Error.data;
+                        };
+                        $scope.Login_click = function () {
+                            $http.get("http://localhost:9810/HMUser", {
+                                params: { "username": $scope.txtusername, "password": $scope.txtpassword }
+                            })
+                            .then(successcallback, errorcallback);
+                        }
+                    });
