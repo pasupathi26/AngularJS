08317dfa83b8d4f69cc5ec5c04af8ca6d0037fc3
pasupathi.g <PASUPATHI.G@ADVAITAMTECH.COM>
2016-05-25 13:45:31 +0530

    Sample Deleted and AngularChart added

---
+++ AngularChart/script/chart.js
@@ -0,0 +1,52 @@
+﻿var app = angular.module('myApp', ['zingchart-angularjs']);
+
+app.controller('MainController', function ($scope) {
+
+    $scope.myJson = {
+        type: "bar",
+        title: {
+            backgroundColor: "transparent",
+            fontColor: "black",
+            text: "Pasupathi"
+        },
+        backgroundColor: "white",
+        series: [
+          {
+              values: [],
+              backgroundColor: "#4DC0CF"
+          }
+        ]
+    };
+
+    $scope.addValues = function () {
+        var val = Math.floor((Math.random() * 10));
+        console.log(val);
+        $scope.myJson.series[0].values.push(val);
+    }
+
+});
+app.controller('myctrl', function ($scope) {
+
+    $scope.myJson = {
+        type: "bar",
+        title: {
+            backgroundColor: "transparent",
+            fontColor: "black",
+            text: "Balram"
+        },
+        backgroundColor: "white",
+        series: [
+          {
+              values: [1, 2, 3, 4],
+              backgroundColor: "#4DC0CF"
+          }
+        ]
+    };
+
+    $scope.addValues = function () {
+        var val = Math.floor((Math.random() * 10));
+        console.log(val);
+        $scope.myJson.series[0].values.push(val);
+    }
+
+});
